254f05767f76b4cfa134665410375d0f4941df31
Tanner Barlow <tanner.barlow12@gmail.com>
2018-09-19 15:49:28 -0700

    Remove async void

--- TaskBoardAssistant/TaskBoardAssistant/Adapters/Trello/Models/TrelloCard.cs
+++ TaskBoardAssistant/TaskBoardAssistant/Adapters/Trello/Models/TrelloCard.cs
@@ -50,7 +50,7 @@ namespace TaskBoardAssistant.Adapters.Trello.Models
             var trelloList = (TrelloList)list;
             Card.List = trelloList.List;
         }
-        public override async void Archive()
+        public override void Archive()
         {
             Card.IsArchived = true;
         }
